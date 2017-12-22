20cfb432fee7c4b10543eadc3c2efaefc0e047af
Sadaka1234 <gonzalo.duran.saez@gmail.com>
2017-12-22 16:15:18 -0300

    Apolicando Matriz

--- main.cpp
+++ main.cpp
@@ -10,21 +10,38 @@ int max(int a, int b)								//Retorna el int más grande (Gracias google por la
 
 
 /*S[0 .. n-1]      m->     <-n*/
-int LongestPalindrome(string S, int n, int L, int R)	//Recibe el String, el largo del string original, posicion del puntero izquierdo, posicion del puntero derecho
+int LongestPalindrome(string S, int n)	//Recibe el String, el largo del string original + 1, posicion del puntero izquierdo, posicion del puntero derecho
 	{
-		
-		
-	if (L == n || R == 0)								//Si el izquierdo llega al final, o el derecho al inicio, end
+	int M[n][n];
+	int L, R;
+	
+	for (L=1; L<=n; L++)
+		{
+		for (R=n; R>=0; R--)
+			{
+		    if (L == n || R == 0)
+				M[L][R] = 0;
+	  
+		    else if (S[R-1] == S[L-1])
+				M[L][R] = M[L+1][R-1] + 1;
+        	  
+		    else
+				M[L][R] = max(M[L+1][R], M[L][R-1]);
+			}
+		}
+	return M[n][0];	
+	}
+	/*
+	if (L == n || R == 0)							
 		{
 		return 0;
 		}
 	if (S[R-1] == S[L-1])								//Si son iguales se avanza (recordar matriz)
 		return 1 + LongestPalindrome(S, n, L+1, R-1);
 	else  												//sino, el máximo entre los otros 2 caminos
-		return max(LongestPalindrome(S, n, L+1, R), LongestPalindrome(S, n, L, R-1));
-		
+		return max(LongestPalindrome(S, n, L+1, R), LongestPalindrome(S, n, L, R-1));	
 	}
-
+	*/
 
 int main(){
 	std::ios::sync_with_stdio(false);
@@ -32,7 +49,7 @@ int main(){
 	int n;												//Largo String
     while (std::getline(std::cin, input)){ 				//Loop hasta el EOF
         n = input.length() + 1;
-		std::cout<< input << LongestPalindrome(input, n, 1, n) << "\n";
+		std::cout<< input << LongestPalindrome(input, n) << "\n";
 		}
 	return 0;
 }
