6872853e0526b75f0bd3402080ecd0b6fc5c4d8f
Ignacio Cofre <ignacio.cofre.14@sansano.usm.cl>
2017-12-22 17:59:23 -0300

    v0.1.0 matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -3,36 +3,63 @@
 #include <iostream>
 using namespace std;
 
-int longo(String s){
-  int n = s.length()+1;
-  int j,i,cont =0, f1[n], f2[n];
+int max(int a, int b)								//Retorna el int más grande (Gracias google por la idea)
+	{
+	return (a > b)? a:b;
+	}
 
-  for(i==0; i<n; i++){
+/*S[0 .. n-1]      L->     <-R*/
+int LongestPalindrome(string s,int n)
+	{
+
+  int j,i,cont =0, f1[n+1], f2[n+1];
+
+  for(i=0; i<n; i++){
     f1[i]=0;
   }
 
   f2[0]=0;
 
-  for(int j =n-2; j >= 0; j--){
-    for(int i = 1; i < n+1; j++){
-      if(cont%2==0){
-        f2[i] = max(f2[i-1],f1[i]);
+  for(j =n-1; j >= 0; j--){
+    for(i = 1; i <= n; i++){
 
-        if(c==palabra[j]){
-          f2[i] +=1;
+      if(cont%2==0){
+        if(s[j]==s[i]){
+          f2[i] = f1[i-1]+ 1;
+        }
+        else{
+          f2[i] = max(f2[i-1],f1[i]);
         }
       }
-      if(cont%2==1){
-        f1[i] = max(f1[i-1],f2[i]);
 
-        if(c==s[j]){
-          f1[i] +=1;
+      else{
+        if(s[j]==s[i]){
+          f1[i] = f2[i-1] + 1;
+        }
+        else{
+          f1[i] = max(f1[i-1],f2[i]);
         }
       }
     }
     cont+=1;
   }
+
   if(cont%2==1){
-    retun
+    return f1[n-1];
   }
+
+  else{
+    return f2[n-1];
+  }
+}
+
+int main(){
+	std::ios::sync_with_stdio(false);
+	string input;										//El String
+	int n;												//Largo String
+    while (std::getline(std::cin, input)){ 				//Loop hasta el EOF
+        n = input.length();
+		std::cout<< LongestPalindrome(input, n) << "\n";
+		}
+	return 0;
 }
