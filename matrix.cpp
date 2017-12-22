6382187eb46ed8d3cca37bb77e250ce95bb15c35
Sadaka1234 <gonzalo.duran.saez@gmail.com>
2017-12-22 18:41:46 -0300

    otro

--- matrix.cpp
+++ matrix.cpp
@@ -11,22 +11,21 @@ int max(int a, int b)								//Retorna el int más grande (Gracias google por la
 /*S[0 .. n-1]      L->     <-R*/
 int LongestPalindrome(string s,int n)
 	{
-  int j,i,cont =0, f1[n+1], f2[n+1];
+  int j,i,cont =0, f1[n], f2[n];
   for(i=0; i<n; i++){
     f1[i]=0;
   }
   f2[0]=0;
-  for(j =n-1; j >= 0; j--){
-    for(i = 1; i <= n; i++){
+  for(j= n ; j >= 0; j--){
+    for(i = 0; i <= n; i++){
       if(cont%2==0){
         if(s[j]==s[i]){
-          f2[i] = f1[i-1]+ 1;
+          f2[i] = f1[i-1]+1;
         }
         else{
           f2[i] = max(f2[i-1],f1[i]);
         }
       }
-
       else{
         if(s[j]==s[i]){
           f1[i] = f2[i-1] + 1;
@@ -39,10 +38,10 @@ int LongestPalindrome(string s,int n)
     cont+=1;
   }
   if(cont%2==1){
-    return f1[n-1];
+    return f2[n-1];
   }
   else{
-    return f2[n-1];
+    return f1[n-1];
   }
 }
 
@@ -52,7 +51,7 @@ int main(){
 	int n;												//Largo String
     while (std::getline(std::cin, input)){ 				//Loop hasta el EOF
         n = input.length();
-		std::cout<< LongestPalindrome(input, n) << "\n";
+		std::cout<< LongestPalindrome(input, n+1) << "\n";
 		}
 	return 0;
 	
