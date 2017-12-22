115643f8bc17b023efe7cd72a5eaa83710cc7203
Sadaka1234 <gonzalo.duran.saez@gmail.com>
2017-12-22 19:27:59 -0300

    Terminamos :)

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 primer:
 	g++ -Wall -o ejecutar matrix.cpp
-	./ejecutar < short-words.txt > output-alumno.txt
+	time ./ejecutar < long-words.txt
--- matrix.cpp
+++ matrix.cpp
@@ -3,56 +3,50 @@
 #include <iostream>
 using namespace std;
 
-int max(int a, int b)								//Retorna el int más grande (Gracias google por la idea)
+int max(int a, int b)					//Retorna el int más grande (Gracias google por la idea)
 	{
 	return (a > b)? a:b;
 	}
 
 /*S[0 .. n-1]      L->     <-R*/
-int LongestPalindrome(string s,int n)
+int LongestPalindrome(string S, int n)	//Recibe el String, el largo del string original + 1
 	{
-  int j,i,cont =0, f1[n], f2[n];
-  for(i=0; i<n; i++){
-    f1[i]=0;
-  }
-  f2[0]=0;
-  for(j= n ; j >= 0; j--){
-    for(i = 0; i <= n; i++){
-      if(cont%2==0){
-        if(s[j]==s[i]){
-          f2[i] = f1[i-1]+1;
-        }
-        else{
-          f2[i] = max(f2[i-1],f1[i]);
-        }
-      }
-      else{
-        if(s[j]==s[i]){
-          f1[i] = f2[i-1] + 1;
-        }
-        else{
-          f1[i] = max(f1[i-1],f2[i]);
-        }
-      }
-    }
-    cont+=1;
-  }
-  if(cont%2==1){
-    return f2[n-1];
-  }
-  else{
-    return f1[n-1];
-  }
-}
+	int M[n][2];						//Se almacenan los datos en 2 arreglos de tamaño n (Uso de memoria O(n))
+	int L, R;
+	for (R=n; R>0; R--)
+		{
+		for (L=0; L<n; L++)
+			{
+			if (R%2 == 0)				//El modulo se utiliza para decidir cual arreglo usar
+				{
+				if (L == 0 || R == n)
+					M[L][0] = 0;
+				else if (S[R-1] == S[L-1])
+					M[L][0] = M[L-1][1] + 1;
+				else
+					M[L][0] = max(M[L-1][0], M[L][1]);
+				}
+			else
+				{	
+				if (L == 0 || R == n)
+					M[L][1] = 0;
+				else if (S[R-1] == S[L-1])
+					M[L][1] = M[L-1][0] + 1;
+				else
+					M[L][1] = max(M[L-1][1], M[L][0]);
+				}
+			}
+		}
+	return max(M[n-1][1],M[n-1][0]);	// Retorna el máximo de los ultimos 2 elementos de los arreglos
+	}
 
 int main(){
 	std::ios::sync_with_stdio(false);
 	string input;										//El String
 	int n;												//Largo String
     while (std::getline(std::cin, input)){ 				//Loop hasta el EOF
-        n = input.length();
-		std::cout<< LongestPalindrome(input, n+1) << "\n";
+        n = input.length() + 1;
+		std::cout<< LongestPalindrome(input, n) << "\n";
 		}
 	return 0;
-	
 }
